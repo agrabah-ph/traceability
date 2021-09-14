59d6855b81891fef0fcab502ade47c3c31bf4a99
frightnight <jasper.garcera@gmail.com>
2021-09-15 05:23:29 +0800

    updates bfar

---
+++ resources/views/trace/bfar/dashboard.blade.php
@@ -0,0 +1,33 @@
+@extends('trace.master')
+
+{{--@section('title', 'Dashboard | '.getRoleName('display_name'))--}}
+@section('title', 'Dashboard')
+
+@section('content')
+
+    <section class="container animated fadeInRight">
+    </section>
+
+    <div class="wrapper wrapper-content">
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+@section('styles')
+    {{--{!! Html::style('') !!}--}}
+@endsection
+
+@section('scripts')
+    {{--{!! Html::script('') !!}--}}
+    <script>
+        $(document).ready(function(){
+
+        });
+    </script>
+@endsection
